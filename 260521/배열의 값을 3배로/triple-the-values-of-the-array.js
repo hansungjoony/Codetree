6bb84103e55170c20a87f63c3aeda87707035176
hansungjoony <hansungjoony@gmail.com>
2026-05-21 17:54:28 +0900

    [Passed] 배열의 값을 3배로 (63ms, 10MB)

---
+++ 260521/배열의 값을 3배로/triple-the-values-of-the-array.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let arr = [
+    [0, 0, 0], 
+    [0, 0, 0], 
+    [0, 0, 0]
+];
+
+for (let i = 0; i < 3; i++) {
+    arr[i] = input[i].split(" ").map(Number);
+}
+
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        arr[i][j] *= 3;
+    }
+}
+
+for(let i = 0; i < 3; i++) {
+    let str = "";
+    for(let j = 0; j < 3; j++) {
+        str += arr[i][j] + " ";
+    }
+    console.log(str);
+}
